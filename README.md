c644907016585af9527d2071e9ec78d9ed6bd430
Andrew Livigni <152040968+DrewBagelz@users.noreply.github.com>
2024-06-27 00:27:56 -0400

    Increment opacity on mouseover

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # odin-etch-a-sketch
 
 Etch-a-Sketch project for The Odin Project.
+
+## Overview
+
+A grid that incrementally darkens its cells on mouseover, similar to
+the classic "Etch a Sketch."
+
+## Reflections
+
+I didn't have much trouble with this project and was able to easily
+find answers to any questions I did have with a little bit of
+Googling. The only thing that gave me grief was how to increment the
+opacity on every mouseover, but it turns out I was just overthinking
+the solution. I should have committed more often, as I pretty much
+just left this project open in a save-state on my virtual machine
+for the entire duration of my working on it.
--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-createGrid(16);
+etchSetUp(16);
 
-function createGrid(squares) {
+function etchSetUp(squares) {
 	for (let i = 0; i < `${squares}` * `${squares}`; i++) {
 		const gridSquare = document.createElement("div");
 		container.appendChild(gridSquare);
-		gridSquare.classList.toggle("grid-square");
+		gridSquare.classList.add("grid-square");
 		let size = 800 / `${squares}` + "px";
 		gridSquare.style.height = size;
 		gridSquare.style.width = size;
+		let opacity = 0;
+		gridSquare.addEventListener("mouseover", () => {
+			opacity += 0.1;
+			gridSquare.style.background = `rgb(0 0 0 / ${opacity})`;
+		});
+	}
+}
+
+function promptUser() {
+	let squares = Number(prompt("Choose number of squares for each axis:", ""));
+	if (squares <= 100) {
+		container.replaceChildren();
+		etchSetUp(squares);
+	} else {
+		let squares = Number(prompt("Please choose a number from 1 to 100:", ""));
+		container.replaceChildren();
+		etchSetUp(squares);
 	}
 }
